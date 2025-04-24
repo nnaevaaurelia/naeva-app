007eb20e174a73a109c01e430113c84162776c5c
nnaevaaurelia <naeva.aureliazahra@gmail.com>
2025-04-24 11:02:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 Latihan membuat Website")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start building! For help and inspiration, head over ."
 )
